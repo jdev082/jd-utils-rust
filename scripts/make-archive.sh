0e1342c29445e739fb51d0a3a72e9752b61f0ad8
JaydenDev <willsjayden2@gmail.com>
2022-06-27 15:19:09 -0500

    feat: add automated archive creation script

---
+++ scripts/make-archive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#makes archive for creating releases
+#source config.in.sh
+
+rm -rf bin/
+pkgs=( "list/" "no/"  )
+ver="v0.1.0"
+cd ../list
+cargo build --release
+cd ../no
+cargo build --release
+cd ..
+mkdir bin
+mv list/target/release/list bin/
+mv no/target/release/no bin/
+tar -cvf archive/jd-utils-$ver.tar bin
