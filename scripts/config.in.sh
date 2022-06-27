58a6aa4ffd9c8a17737640bba5e6a5dadc9b88ef
JaydenDev <willsjayden2@gmail.com>
2022-06-27 15:42:08 -0500

    feat: add config file

---
+++ scripts/config.in.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#build config
+export ver="0.1.0"
--- scripts/make-archive.sh
+++ scripts/make-archive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #makes archive for creating releases
-#source config.in.sh
+source config.in.sh
 
 rm -rf bin/
-pkgs=( "list/" "no/"  )
-ver="v0.1.0"
+#pkgs=( "list/" "no/"  )
+#ver="v0.1.0"
 cd ../list
 cargo build --release
 cd ../no
@@ -14,3 +14,4 @@ mkdir bin
 mv list/target/release/list bin/
 mv no/target/release/no bin/
 tar -cvf archive/jd-utils-$ver.tar bin
+echo created jd-utils-$ver.tar from bin/ to archive/
